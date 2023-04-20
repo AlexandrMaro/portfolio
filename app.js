ba2039afa26a17d9d3fd479b94b8f99fc242b457
Alexander <gale249@gmail.com>
2023-04-20 08:38:05 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,113 @@
+const header = document.querySelector('header');
+
+function checkScrollPosition() {
+  let scrollPosition = window.scrollY;
+  if (scrollPosition > 70) {
+    header.classList.add('red', 'scroll-header');
+  } else {
+    header.classList.remove('red', 'scroll-header');
+  }
+}
+
+document.addEventListener('DOMContentLoaded', checkScrollPosition);
+window.addEventListener('scroll', checkScrollPosition);
+
+const langArr = {
+  title: {
+    eng: 'Portfolio Website',
+    ru: 'Сайт портфолио',
+  },
+  menuMain: {
+    eng: 'Main',
+    ru: 'Главная',
+  },
+  menuPortfolio: {
+    eng: 'Portfolio',
+    ru: 'Портфолио',
+  },
+  menuAbout: {
+    eng: 'About',
+    ru: 'Обо мне',
+  },
+  menuContacts: {
+    eng: 'Contacts',
+    ru: 'Контакты',
+  },
+  textHi: {
+    eng: 'Hi, my name is',
+    ru: 'Привет, меня зовут',
+  },
+  textName: {
+    eng: 'Alex',
+    ru: 'Алекс',
+  },
+  textInfo: {
+    eng: "and i'm student at Estonian Entrepreneurship University of Applied Sciences",
+    ru: 'и я студент Эстонского Университета Предпринимательства Майнор',
+  },
+  portfolio: {
+    eng: '{ Portfolio }',
+    ru: '{ Портфолио }',
+  },
+  about: {
+    eng: '{ AboutMe }',
+    ru: '{ Обо мне }',
+  },
+  contact: {
+    eng: '{ ContactMe }',
+    ru: '{ Мои Контакты }',
+  },
+  ContactName: {
+    eng: 'Name',
+    ru: 'Имя',
+  },
+  ContactAdress: {
+    eng: 'Adress',
+    ru: 'Адрес',
+  },
+  ContactPhone: {
+    eng: 'Phone',
+    ru: 'Номер телефона',
+  },
+  ContactMsg: {
+    eng: 'Message Me',
+    ru: 'Свяжитесь со мной',
+  },
+  sendBtn: {
+    eng: 'Send',
+    ru: 'Отправить',
+  },
+};
+
+const select = document.querySelector('select');
+select.addEventListener('change', changeUrlLanguage);
+
+const allLangArr = ['eng', 'ru'];
+
+function changeUrlLanguage() {
+  let lang = select.value;
+  location.href = window.location.pathname + '#' + lang;
+  console.log(lang);
+  location.reload();
+}
+
+function changeLanguage() {
+  let hash = window.location.hash;
+  hash = hash.substring(1);
+  if (!allLangArr.includes(hash)) {
+    location.href = window.location.pathname + '#eng';
+    location.reload();
+  }
+
+  select.value = hash;
+  document.querySelector('title').innerHTML = langArr['title'][hash];
+
+  for (let key in langArr) {
+    let elem = document.querySelector('.lng-' + key);
+    if (elem) {
+      elem.innerHTML = langArr[key][hash];
+    }
+  }
+}
+
+changeLanguage();
